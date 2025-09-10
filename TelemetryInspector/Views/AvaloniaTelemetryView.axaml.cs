953d4ef287155fcf8ab1f3af18145d160ab527f5
Mike James <michael.james6@gmail.com>
2025-09-10 11:24:59 +0200

    Started to add a telemetry viewer app

---
+++ TelemetryInspector/Views/AvaloniaTelemetryView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TelemetryInspector.Views;
+
+public partial class AvaloniaTelemetryView : UserControl
+{
+    public AvaloniaTelemetryView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TelemetryInspector/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TelemetryInspector.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
